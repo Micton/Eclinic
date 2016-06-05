139cf8d59a7bf906c6e5b2f24bd6344bada8a46f
Taras Martsyniak <micton86@gmail.com>
2016-06-05 16:58:47 +0300

    commit from IDEA

--- src/main/java/com/eclinic/Clinic.java
+++ src/main/java/com/eclinic/Clinic.java
@@ -2,16 +2,12 @@ package com.eclinic;
 
 
 
-import com.eclinic.model.Patient;
-import lombok.val;
-
-import java.util.ArrayList;
 
 public class Clinic {
 
 
     public static void main(String[] args)  {
 
-xxxxxxxxxxxxxx
+
     }
 }
